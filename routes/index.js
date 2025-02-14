a435ea9426fe2695a3561edd46a3ee86e554d5b2
Bernspecial <michaelbernardolayemi@gmail.com>
2025-02-14 10:39:06 +0100

    Authorization Code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
+const passport = require("passport");
+
 const routes = require('express').Router();
 
 routes.use('/', require('./swagger'));
 routes.get('/', (req, res) => {
   // swagger.tags=['Hello World']
   res.send('Hello World');
+});
+
+routes.get("/login", passport.authenticate("github"), (req, res) => { })
+routes.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect("/");
+  });
 });
 
 routes.use('/user', require('./user'));
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 const routes = require('express').Router();
 const productController = require('../controllers/product');
 const validation = require('../middleware/validate');
+const { isAuthenticated } = require("../middleware/authenticate");
 
 routes.get('/', productController.getAll);
 routes.get('/:id', productController.getSingle);
-routes.post('/', validation.saveProduct, productController.createProduct);
-routes.put('/:id', validation.saveProduct, productController.updateProduct);
-routes.delete('/:id', productController.deleteProduct);
+routes.post('/', isAuthenticated, validation.saveProduct, productController.createProduct);
+routes.put('/:id', isAuthenticated, validation.saveProduct, productController.updateProduct);
+routes.delete('/:id', isAuthenticated, productController.deleteProduct);
 
 module.exports = routes;
