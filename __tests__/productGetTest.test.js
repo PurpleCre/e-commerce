11150399a15594af8fc3d4e48f5f2c779264371c
Bernspecial <michaelbernardolayemi@gmail.com>
2025-02-19 19:45:21 +0100

    Testing test unit

--- __tests__/productGetTest.test.js
+++ __tests__/productGetTest.test.js
@@ -34,13 +34,22 @@ describe("User  API", () => {
         });
     });
 
+
+    describe("GET /product/stock", () => {
+        it("should return all products", async () => {
+            const response = await request(app).get('/product/stock');
+            expect(response.statusCode).toBe(200);
+            expect(Array.isArray(response.body)).toBe(true);
+        });
+    });
+
     describe("GET /user/:id", () => {
         it("should return a single user by id", async () => {
             const userId = "67ac7dc2da1b26eef624bc27"; // Use a valid user ID from your database
             const response = await request(app).get(`/product/${userId}`);
 
             expect(response.statusCode).toBe(200);
-            
+
         });
         it("should return 400 for invalid product id", async () => {
             const userId = "67ac7dc2da1b26eef624bc27"; // Use a valid user ID from your database
